d78805b0b7d84c122ef4baf258d9d265bce1c87f
thangsuperman <thangjenny2002@gmail.com>
2025-01-04 13:44:58 +0700

    Migration node package manager from pnpm to yarn and add jest testing framework

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Use ts-jest preset for testing TypeScript files with Jest
+  preset: "ts-jest",
+
+  // Set the test environment to Node.js
+  testEnvironment: "node",
+
+  // Define the root directory for tests and modules
+  roots: ["<rootDir>/src/tests"],
+
+  // Use ts-jest to transform TypeScript files
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+
+  testRegex: "((\\.|/)(test|spec))\\.ts?$",
+
+  // File extensions to recognize in module resolution
+  moduleFileExtensions: ["ts", "json", "node"],
+};
