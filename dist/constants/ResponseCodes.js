bc47effa80ba3c4e98821db86ab8f0d62db3ec41
thangsuperman <thangjenny2002@gmail.com>
2025-01-04 17:10:14 +0700

    Remove dist folder

--- dist/constants/ResponseCodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RESPONSE_CODES = {
-    SUCCESS: 200,
-    CREATED: 201,
-    NOT_FOUND: 404,
-    ERROR_INTERNAL_SERVER: 404,
-};
-exports.default = RESPONSE_CODES;
