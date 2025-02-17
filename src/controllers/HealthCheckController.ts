9b3727c6a595d603e1613141e5eeb556c2376f5b
thangsuperman <thangjenny2002@gmail.com>
2025-02-17 23:06:28 +0700

    Improve health check API endpoint by checking writer and reader database instance

--- src/controllers/HealthCheckController.ts
+++ src/controllers/HealthCheckController.ts
@@ -1,8 +1,44 @@
+/* eslint-disable no-console */
 import { Request, Response } from 'express';
 import { ResponseUtil } from '../utils/Response';
+import { DataSource, QueryRunner } from 'typeorm';
+import { AppDataSource } from '../database/data-source';
 
 export class HealthCheckController {
   async checkHealth(req: Request, res: Response) {
+    let queryRunner: QueryRunner | null = null;
+
+    try {
+      const dataSource: DataSource = AppDataSource;
+      if (!dataSource.isInitialized) {
+        AppDataSource.initialize()
+          .then(async () => {
+            console.log('Database connection success');
+          })
+          .catch((err) => console.log(err));
+      }
+
+      const readResult = await dataSource.query('SELECT 1 AS read_check');
+      if (!readResult || readResult.length === 0) {
+        throw new Error('Read health check failed');
+      }
+
+      queryRunner = dataSource.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+      await queryRunner.query("INSERT INTO health_check (status, created_at) VALUES ('ok', NOW());");
+      await queryRunner.rollbackTransaction();
+    } catch (err: unknown) {
+      console.log(`Error health check: ${err}`);
+
+      ResponseUtil.sendError({
+        res,
+        message: 'Internal server error'
+      });
+
+      return;
+    }
+
     ResponseUtil.sendResponse({
       res,
       data: null
