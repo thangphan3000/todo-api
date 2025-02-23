844424027843c27ded192a7c06fd4253099af16f
thangsuperman <thangjenny2002@gmail.com>
2025-02-23 13:45:39 +0700

    Implement logger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import 'reflect-metadata';
 import app from './app';
 import { AppDataSource } from './database/data-source';
+import log from './utils/Logger';
 
 dotenv.config();
 
@@ -10,10 +11,10 @@ const PORT = Number(process.env.APP_PORT) || 3000;
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log('Database connection success');
+    log.info('Database connection success');
   })
   .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on the port: ${PORT}`);
+  log.info(`Server is listening on the port: ${PORT}`);
 });
