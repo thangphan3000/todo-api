f6fd879fdd7ef2bc04fb25fbafe0179023455e9a
Thang Phan <thangjenny2002@gmail.com>
2025-03-26 22:24:44 +0700

    Fix missing port

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,20 +1,34 @@
 import winston from 'winston';
-const { combine, timestamp, json, errors } = winston.format;
 
+const { combine, colorize, timestamp, json, errors, cli } = winston.format;
 const LOGS_FOLDER_PATH = 'logs/';
 
-const transports = [
-  new winston.transports.File({ filename: `${LOGS_FOLDER_PATH}/standards.log` }),
-  ...(process.env.NODE_ENV === 'development' ? [new winston.transports.Console()] : [])
-];
+const tags = { development: 'local', staging: 'staging', uat: 'uat', production: 'prod' };
+
+const consoleTransport = new winston.transports.Console({
+  format: combine(
+    colorize(),
+    cli({
+      colors: {
+        error: 'red',
+        warn: 'yellow',
+        info: 'blue',
+        http: 'green',
+        verbose: 'cyan',
+        debug: 'white'
+      }
+    })
+  ),
+  handleExceptions: true
+});
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(timestamp(), errors({ stack: true }), json()),
-  transports,
+  transports: [new winston.transports.File({ filename: `${LOGS_FOLDER_PATH}/standards.log` }), consoleTransport],
   exceptionHandlers: [new winston.transports.File({ filename: `${LOGS_FOLDER_PATH}/exceptions.log` })],
   rejectionHandlers: [new winston.transports.File({ filename: `${LOGS_FOLDER_PATH}/rejections.log` })],
-  defaultMeta: { app: 'nodejs' }
+  defaultMeta: { app: 'nodejs', env: tags[process.env.NODE_ENV || 'development'] }
 });
 
 export default logger;
